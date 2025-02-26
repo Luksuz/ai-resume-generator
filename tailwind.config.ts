a29f1388aa3e7b5b55aa52e69fadd0d6731bfcbe
luksuz <lukamindjek@gmail.com>
2025-02-26 22:51:12 +0100

    remove eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	  },
+	  typescript: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has TypeScript errors.
+		ignoreBuildErrors: true
+	},
+	darkMode: ["class"],
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
