0b36979bf8e24dae592f55c560f3a2604b5ea358
luksuz <lukamindjek@gmail.com>
2025-02-27 10:26:01 +0100

    removed server routes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle .ts files in node_modules
+    config.module.rules.push({
+      test: /\.ts$/,
+      include: /node_modules\/chrome-aws-lambda/,
+      use: 'ts-loader',
+    });
+
+    return config;
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
